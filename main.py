60172ef23efda30872dea7958a46144183696696
bobbyhadz <opmftw@gmail.com>
2024-04-09 15:15:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Round a float to 1, 2 or 3 decimal places in Python
+
+my_float = 6.36789
+
+# ✅ round a float to 1, 2 or 3 decimal places (round())
+
+result = round(my_float, 1)
+print(result)  # 👉️ 6.4
+
+result = round(my_float, 2)
+print(result)  # 👉️ 6.37
+
+result = round(my_float, 3)
+print(result)  # 👉️ 6.368
